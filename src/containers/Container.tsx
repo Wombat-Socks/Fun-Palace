9c678f19bd10b9bc59cf8bcc0f3131704cf0ac06
Black-Tomcat <tomcat.frantz@gmail.com>
2019-11-25 21:24:09 +1100

    Added skeleton of the container component

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -1,3 +1,15 @@
 /*
 Personalised container much like our own div
  */
+import React, {useState} from "react";
+import {View, ViewProps} from "react-native";
+
+interface ContainerProps extends ViewProps {
+    children?: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = (props: ContainerProps) => {
+    return (<View style={props.style}>{props.children}</View>)
+};
+
+export default Container;
